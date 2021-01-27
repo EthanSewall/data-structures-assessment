078be62057a7d42eec1d9b3210e78c70d801cc9a
Ethan <lemonmitts@gmail.com>
2021-01-27 14:06:02 -0800

    recursion

--- binary tree/BinaryTree/tree.h
+++ binary tree/BinaryTree/tree.h
@@ -19,6 +19,8 @@ public:
 
 	void Insertion(T &value);
 
+	void RecursiveInsertion(Vertex*& current, T& value);
+
 private:
 	std::vector<Vertex*> vertices;
 
@@ -26,3 +28,50 @@ private:
 
 
 };
+
+template <typename T>
+BinaryTree<T>::BinaryTree()
+{
+	root = nullptr;
+}
+
+template <typename T>
+BinaryTree<T>::~BinaryTree()
+{
+	for each (Vertex v in vertices)
+	{
+		delete v;
+	}
+}
+
+template <typename T>
+void BinaryTree<T>::Insertion(T& value)
+{
+	RecursiveInsertion(root, value);
+}
+
+template <typename T>
+void BinaryTree<T>::RecursiveInsertion(Vertex*& current, T& value)
+{
+	if (current == nullptr)
+	{
+		current = new Vertex();
+		current->data = T;
+		vertices.push_back(current);
+	}
+	else
+	{
+		if (current->data == value)
+		{
+			//ignore this
+		}
+		else if (value < current->data)
+		{
+			RecursiveInsertion(current->left, value);
+		}
+		else
+		{
+			RecursiveInsertion(current->right, value);
+		}
+	}
+}
